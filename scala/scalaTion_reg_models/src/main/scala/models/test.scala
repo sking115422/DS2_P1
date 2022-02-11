58f5e7164ebcbadd13266662f173a74ae7f36908
sking115422 <spencerd.king@gmail.com>
2022-02-10 22:16:20 -0500

    added data folder so we can read data in scala

---
+++ scala/scalaTion_reg_models/src/main/scala/models/test.scala
@@ -0,0 +1,14 @@
+package scalation
+package modeling
+
+import scala.runtime.ScalaRunTime.stringOf
+
+import scalation.mathstat._
+
+
+@main def test (): Unit = {
+
+
+    MatrixD.load("AirQualityUCI_fixed_cleaned.csv")
+
+}
