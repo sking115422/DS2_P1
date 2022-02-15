ee001900bc95a1f6c4550da9ed932e32bf992039
Nikhil Jindia <nikhiljindia@gmail.com>
2022-02-14 19:22:05 -0500

    Forward selection done? (high r2cv)

--- python/Ridge_Regression.py
+++ python/Ridge_Regression.py
@@ -1,7 +1,7 @@
-from sklearn.linear_model import ridge_regression
+from sklearn import linear_model
 from Model_Parent import *
 aq_df = get_air_quality_df()
 ampg_df = get_auto_mpg_df()
 ff_df = get_forest_fires_df()
 
-rr = ridge_regression()
+forward_selection(linear_model.Ridge(), ff_df.iloc[:,:-1], ff_df.iloc[:,1])
